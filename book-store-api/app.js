b3784db90f9b98b35e65563e358a944614fe5686
selkhadr <elkhadrisara01@gmail.com>
2025-06-09 17:35:17 +0100

    add post book with joi validation

--- book-store-api/app.js
+++ book-store-api/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Joi = require('joi');
 const app = express();
 
 app.use(express.json());
@@ -33,14 +34,29 @@ app.get("/api/books/:id", (req, res) => {
 })
 
 app.post("/api/books", (req, res) => {
-    console.log(req.body);
+    const schema = Joi.object({
+        name: Joi.string().trim().min(3).max(200).required(),
+        author: Joi.string().min(3).max(200).required()
+    });
+
+    const { error, value } = schema.validate(req.body); // Correct extraction
+
+    if (error) {
+        console.log("\n\n");
+        console.log(error.details[0].message);
+        console.log("\n\n");
+        return res.status(400).json({ message: error.details[0].message });
+    }
+
     const book = {
         id: books.length + 1,
-        name: req.body.name,
-        author: req.body.author
-    }
+        name: value.name, // Use validated `value` instead of raw `req.body`
+        author: value.author
+    };
+
     books.push(book);
     res.status(201).json(book);
-})
+});
+
 
 app.listen(5000, ()=>console.log("server start"));
